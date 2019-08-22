b14c1ff8db1e9f72d1ac6aaddf1461cec7a5773a
raphael <raphaelcrv@hotmail.com>
2019-08-22 02:57:01 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 //load external module
-const autoGenerate = require('./controller/generator')
-
-//Global Params
-global.urlApi = 'https://dev78974.service-now.com/api/now/table/'
-
+const catalogGenerator = require('./controller/catalog-generator')
 
 
 /**
- * Function: createCatalogObjects 
+ * Function: catalogGenerator 
  * Desc: Gera automaticamente categorias, sub, items
  */
 
-autoGenerate.createCatalogObjects({
-	fileName : 'data.xlsx',
+
+catalogGenerator.createCatalogCategoriesByExcel({
+	filePath : 'data.xlsx',
 	catalogSysId : '0410c0e8db323300ccc961ca0b9619e1',
 	variableSet : ''
 })
+
